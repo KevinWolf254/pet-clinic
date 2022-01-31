e425dfb6de1a3c268545ab4c540fa9384fe169ef
KevinWolf254 <mbugua.kevinkanyi@gmail.com>
2022-01-31 08:12:26 +0300

    added map based service. closes #6

---
+++ pet-clinic-data/src/main/java/co/ke/proaktivio/petclinic/services/map/VetMapService.java
@@ -0,0 +1,33 @@
+package co.ke.proaktivio.petclinic.services.map;
+
+import co.ke.proaktivio.petclinic.models.Vet;
+import co.ke.proaktivio.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
